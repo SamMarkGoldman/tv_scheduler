b3639e55572db37d0348873fd6e3c18ea13cb4f7
Sam Goldman <sam@samgoldman.net>
2017-02-12 15:34:22 -0500

    using the irsend API correctly

--- remote.rb
+++ remote.rb
@@ -4,10 +4,8 @@ class Remote
   end
 
   def self.channel(channel_string)
-  Logger.info "setting channel to #{channel_string}"
-    channel_string.each_char do |c|
-      `irsend SEND_ONCE #{AppConfig::REMOTE_NAME} KEY_#{c}`
-      sleep 0.2
-    end
+    Logger.info "setting channel to #{channel_string}"
+    command = channel_string.chars.map { |c| "KEY_#{c}" }.join(' ')
+    `irsend SEND_ONCE #{AppConfig::REMOTE_NAME} #{command}`
   end
 end
